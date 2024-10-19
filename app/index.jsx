624914d424c06db26b39a339762e72eb7e9d6380
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-10-19 14:47:24 +0300

    bottom bar

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ export default function App() {
         return (
                 <View className="flex-1 items-center justify-center bg-white">
                         <Text className="font-pblack">Aurora</Text>
-                        <Link href="/profile">Go to Profile</Link>
+                        <Link href="/home">Go to Home</Link>
                 </View>
         );
 }
