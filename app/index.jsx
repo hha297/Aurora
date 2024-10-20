1bd1286beba691b877ebc50d9e75a3ac0af8d2f4
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-10-20 13:26:50 +0300

    Onboarding Screen & Custom Button

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,28 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Redirect, router } from 'expo-router';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton.jsx';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
         return (
-                <View className="flex-1 items-center justify-center bg-white">
-                        <Text className="font-pblack">Aurora</Text>
-                        <Link href="/home">Go to Home</Link>
-                </View>
+                <SafeAreaView className="bg-primary h-full">
+                        <ScrollView contentContainerStyle={{ height: '100%' }}>
+                                <View className="w-full justify-center items-center min-h-[90vh] px-4">
+                                        <Image source={images.logo} className="w-32 h-20" resizeMode="contain" />
+                                        <Image source={images.cards} className="max-w-sm w-full h-72" resizeMode="contain" />
+                                        <View className="relative mx-5">
+                                                <Text className="text-3xl text-white font-pbold text-center">
+                                                        Discover Endless Possibilities With <Text className=" text-secondary font-bold ">Aora</Text>
+                                                </Text>
+                                                <Image source={images.path} className="w-32 h-4 absolute -bottom-2 -right-8" resizeMode="contain" />
+                                        </View>
+                                        <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration</Text>
+                                        <CustomButton title="Continue with Email" handlePress={() => router.push('/sign-in')} containerStyles="w-full mt-6" />
+                                </View>
+                        </ScrollView>
+                        <StatusBar backgroundColor="#161622" style="light" />
+                </SafeAreaView>
         );
 }
